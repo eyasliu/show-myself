cdcc33900e9f4ddd853d4a7f2fd064bc61a54dde
liuyuesong <liuyuesong@kingsoft.com>
2016-08-28 18:22:33 +0800

    add session

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 const auth = new Router();
 
 auth.get('/login', async function(c, next){
+	console.log('session: ', c.session)
 	c.body = 'no login';
 })
 
--- server/index.js
+++ server/index.js
@@ -3,16 +3,22 @@ import koa from 'koa';
 import compose from 'koa-compose';
 import convert from 'koa-convert';
 import Router from 'koa-router';
+import session from 'koa-session';
 
 import config from 'root/config';
 import policies, {beforeController, afterController} from './policies';
 import controllers from './controllers';
 
-global.AppConfig = config;
 const app = new koa();
 
+// 暴露出全局变量，方便使用
+global.AppConfig = config;
+global.App = app;
+
+app.keys = ['af78sd68f7s6g6g875ds78fadsfdf76sdfg5asd6f7ds56gf7d5s7g5ds78f578asdf8asd']
 app.use(compose([
 	beforeController,
+	convert(session(app)),
 	controllers,
 	afterController
 ]))
