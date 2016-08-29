2237a4a2b04c86846824acfd6da1bd71728c3fa7
Eyas <liuyuesongde@163.com>
2016-08-29 11:51:19 +0800

    local config

--- config/index.js
+++ config/index.js
@@ -1,14 +1,25 @@
 import serverWebpack from './webpack.server';
 import clientWebpack from './webpack.client';
+import fs from 'fs';
+
+let localConfig = {
+	client: {},
+	server: {}
+}
+try{
+	localConfig = Object.assign(localConfig, JSON.parse(fs.readFileSync('config/local.json').toString()))
+} catch(e){}
 
 export default {
 	host: '127.0.0.1',
 	client: {
 		port: 3000,
-		webpack: clientWebpack
+		webpack: clientWebpack,
+		...localConfig.client
 	},
 	server: {
 		port: 8000,
-		webpack: serverWebpack
+		webpack: serverWebpack,
+		...localConfig.server
 	}
 }
